bcbc8b9dfea05c684326c0b5e276e75fbc75ab6b
Frankie Reyna <elmacho8000@gmail.com>
2024-10-31 23:23:03 -0700

    added breaks, fillers, error messages, instructions, cleaned up segment code

--- MidLevel/segassignment.py
+++ MidLevel/segassignment.py
@@ -1,11 +1,11 @@
 import os
 import random
 from MidLevel.segment import dirinfo, imginfo, Segment
-from MidLevel.LowLevel.present import present_img
-from psychopy import visual
+from MidLevel.LowLevel.present import present_img, present_instruction
 import pandas as pd
-import psychopy.gui as psygui
+import datetime
 
+#revamp this to just use a CSV file that records segment, val, pres type
 
 def pres_assign(imgdir):
 
@@ -13,22 +13,20 @@ def pres_assign(imgdir):
     "imgdir: directory of images to assign"
 
     #Create dictionary to store images in their sections (directory name/spaced or mass)
-
-    dict = {}
+    df = pd.DataFrame()
     for val in os.listdir(imgdir):
-        imgdirs = os.listdir(os.path.join(imgdir, val))
+        valpath = os.path.join(imgdir, val)
+        imgdirs = os.listdir(valpath)
+        random.shuffle(imgdirs)
         mOs = ''
-        for x in range(len(imgdirs)):
-            img = random.choice(imgdirs)
+        x=0
+        for img in imgdirs:
             mOs =  "spaced" if x % 2 == 0 else "mass"
-            direntry = dirinfo(img, val, mOs, os.path.join(imgdir, os.path.join(val, img)))
-            imgdirs.remove(img)
-            try:
-                dict[os.path.join(val, mOs)].append(direntry)
-            except KeyError:
-                dict[os.path.join(val, mOs)] = [direntry]
+            x += 1
+            df = pd.concat([df, pd.DataFrame(data=[dirinfo(img, val, mOs, os.path.join(valpath, img))])], ignore_index=True) #you actually do need to include the path
+    print(df)
         
-    return(dict)
+    return(df)
 
 def seg_assign(imgdir, m_per_seg, num_segs):
 
@@ -36,7 +34,8 @@ def seg_assign(imgdir, m_per_seg, num_segs):
     "For example, m_per_seg = 2 means (2massNeg, 2massNeu, 2spcNeg, 2spcNeu). Num_segs is how many individual segments there are."
     "The num_segs determines number of segments. This should mach the number of copies made for each image so spaced images are consistent."
 
-    dict = pres_assign(imgdir)
+    df = pres_assign(imgdir)
+    
     numvars = num_segs
 
     segs = []
@@ -44,54 +43,71 @@ def seg_assign(imgdir, m_per_seg, num_segs):
     for x in range(num_segs):
         segs.append(Segment(numvars))
         
-    dng = dict['Negative\\mass']
-    dne = dict['Neutral\\mass']
+    dng = df.loc[(df['val'] == "Negative") & (df['pres'] == 'mass')]
+    dne = df.loc[(df['val'] == "Neutral") & (df['pres'] == 'mass')]
         
     #runs through each segment, give each segment number of massed and spaced blocks needed
 
+    y = 0
     for x in range(num_segs):
         
         for _ in range(m_per_seg):
-            segs[x].add_mblock(dng[0])
-            del dng[0]
-            segs[x].add_mblock(dne[0])
-            del dne[0]
+            segs[x].add_mblock(dng.iloc[y])
+            segs[x].add_mblock(dne.iloc[y])
+            y += 1
 
-    for x in range(len(dict['Negative\\spaced'])):
+    dng = df.loc[(df['val'] == "Negative") & (df['pres'] == 'spaced')]
+    dne = df.loc[(df['val'] == "Neutral") & (df['pres'] == 'spaced')]
+
+    for x in range(dng.shape[0]):
         for y in range(num_segs):
-            segs[y].add_sblock(dict['Negative\\spaced'][x], y)
-            segs[y].add_sblock(dict['Neutral\\spaced'][x], y)
+            segs[y].add_sblock(dng.iloc[x], y)
+            segs[y].add_sblock(dne.iloc[x], y)
 
-    return segs
+    return df, segs
 
-def present_segs(segs, PRACMODE):
+def present_segs(win, segs, segfills, partnum, PRACMODE, BREAK=False, SEGSPERBREAK=0, BREAKSLIDEPATH=""):
 
     #Get participant ID
 
-    participant = {"Participant #": ""}
-    pnum = psygui.DlgFromDict(participant)
-    win = visual.Window(size=(1920, 1080), units="pix", color="white")
+
     nsegs = list(segs)
     df = pd.DataFrame()
 
     #Go thorugh each seg, randomly presenting blocks of each segment
-
-    for i in range(len(nsegs)):
+    segcount = 0
+    fillerstart = 0
+    for _ in range(len(nsegs)):
+        df = present_fillers(win, df, segfills, fillerstart, partnum, PRACMODE)
+        fillerstart += segfills
+        if segcount == SEGSPERBREAK and BREAK == True:
+        #initiate break mode:
+            segcount = 0
+            present_instruction(win, BREAKSLIDEPATH, 5)
         idx = random.randint(0, len(nsegs) - 1)
         seglist = nsegs[idx].get_blocks()
         del nsegs[idx]
-        for j in range(len(seglist)):
+        segcount += 1
+        for _ in range(len(seglist)):
             idx = random.randint(0, len(seglist) - 1)
             block = seglist[idx]
             del seglist[idx]
-            for k in range(len(block)):
+            for _ in range(len(block)):
                 idx = random.randint(0, len(block) - 1)
                 img = block[idx]
                 score, rt = present_img(win, img.path, PRACMODE)
-                df = pd.concat([df, pd.DataFrame({"partinum" : participant['Participant #'], "orgimg" : img.dirname, "imgname" : img.name, "valScore" : score
-                                                  ,"resptime" : rt, "val" : img.val, "pres" : img.pres, "path" : img.path}, index = [0])], ignore_index=True)
+                df = pd.concat([df, pd.DataFrame({"partinum" : partnum, "orgimg" : img.dirname, "imgname" : img.name, "valScore" : score
+                                                  ,"resptime" : rt, "val" : img.val, "pres" : img.pres, "path" : img.path, "date" : datetime.datetime.now()}, index = [0])], ignore_index=True)
                 del block[idx]
 
-    return df, participant
+    return df
 
+def present_fillers(win, df, num_pres, start, partnum, PRACMODE):
+    for img in os.listdir("fillers")[start:start+num_pres]:
+        print(img)
+        path =  os.path.join("fillers", img)
+        score, rt = present_img(win, path, PRACMODE)
+        df = pd.concat([df, pd.DataFrame({"partinum" : partnum, "orgimg" : "N/A", "imgname" : img, "valScore" : score
+                                    ,"resptime" : rt, "val" : "fill", "pres" : "fill", "path" : path, "date" : datetime.datetime.now()}, index = [0])], ignore_index=True)
+    return df
 
